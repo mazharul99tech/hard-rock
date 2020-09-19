cd0573bda3520705ae4ec9e21a08d0fceb60730a
mazharul99tech <mazharul99tech@gmail.com>
2020-09-19 13:40:23 +0600

    lyric added

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const getApi = "https://api.lyrics.ovh/suggest/";
 
 const getLyrics = lyrics => {
-    const apiUrl = `${getApi}${lyrics}`
+    const apiUrl = `${getApi}${lyrics}`;
     fetch(apiUrl)
     .then(res => res.json())
     .then(data => {
@@ -12,12 +12,12 @@ const getLyrics = lyrics => {
         lyrics = lyrics.slice(0, 10);
         for (let i = 0; i < lyrics.length; i++) {
             const lyric = lyrics[i];
-            const album = lyric.album.title;
+            const artist = lyric.artist.name;
             const lyricTitle = lyric.title;
             const p = document.createElement('p');
             p.innerHTML = `
-            <span class="span-text">${lyricTitle} Album By ${album}</span> 
-            <button class ="get-lyric-button">Get Lyrics</button>
+            <span class="span-text">${lyricTitle} Album By ${artist}</span> 
+            <button onclick="loadLyricData('${artist}', '${lyricTitle}')" class ="get-lyric-button">Get Lyrics</button>
             `;
             lyricData.appendChild(p);
             
@@ -25,6 +25,15 @@ const getLyrics = lyrics => {
     })
 }
 
+const loadLyricData = (artist, title)=>{
+    console.log(artist, title)
+        fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                document.getElementById("show-lyrics").innerHTML = `<h6>${data.lyrics}</h6>`;
+            })
+}
 
 
 document.getElementById('search-btn').addEventListener('click', function(){
